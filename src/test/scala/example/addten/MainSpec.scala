698a6a7f68df0c9ddb20fc776fb396dc6da0443c
Chris Davenport <chris@christopherdavenport.tech>
2017-12-12 14:41:42 -0500

    Workaround for Addition Cap at MaxValue

--- src/test/scala/example/addten/MainSpec.scala
+++ src/test/scala/example/addten/MainSpec.scala
@@ -1,7 +1,6 @@
 package example.addten
 
 import org.specs2._
-import scala.util.Try
 import implicits._
 import org.scalacheck._
 
@@ -9,14 +8,19 @@ object MainSpec extends Spec with ScalaCheck {
 
   override def is =
     s2"""
-        | AddTen Int ${lawsForAddTen[Int]}
-        | AddTen String ${lawsForAddTen[String]}"""
+  AddTen Int ${lawsForAddTen[Int]}
+  AddTen String ${lawsForAddTen[String]}
+      """
 
 
 
   def lawsForAddTen[A](implicit A: AddTen[A], E: Enumerated[A], Arb: Arbitrary[A]): Prop =
     Prop.forAll {a: A =>
-      a.addTen must_=== Try(a.enumValue + 10).toOption
+     val result : Option[Int] =
+       if ((a.enumValue.toLong + 10.toLong) > Int.MaxValue.toLong) None
+       else Option(a.enumValue + 10)
+
+      a.addTen must_=== result
     }
 
 
