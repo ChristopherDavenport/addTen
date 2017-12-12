02c10a0371a6dc508c51f8722503f2a20cec86a4
Chris Davenport <chris@christopherdavenport.tech>
2017-12-12 15:01:38 -0500

    Add More Demonstrative Examples to Main

--- src/main/scala/example/addten/Main.scala
+++ src/main/scala/example/addten/Main.scala
@@ -7,7 +7,9 @@ object Main {
   def main(args: Array[String]): Unit = {
     println(10.addTen)
     println("Hello".addTen)
-    println((Int.MaxValue - 11).addTen)
+    println((Int.MaxValue - 10).addTen)
+    println((Int.MaxValue - 9).addTen)
+    println(Int.MaxValue.addTen)
     println(Int.MinValue.addTen)
   }
 
